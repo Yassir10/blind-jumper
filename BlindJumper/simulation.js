f3c871c79c620cd749b5c89d0ba939d11cd65fe8
Gerd Wagner <G.Wagner@b-tu.de>
2019-07-17 23:36:46 +0200

    add observation UI

--- BlindJumper/simulation.js
+++ BlindJumper/simulation.js
@@ -10,6 +10,7 @@
 sim.scenario.simulationEndTime = 50;
 //sim.scenario.randomSeed = 5;  // optional
 sim.config.createLog = true;
+sim.config.visualize = true;
 
 /*******************************************************
  Simulation Model
@@ -25,8 +26,8 @@ sim.model.v.alpha=0.2;
  Define the initial state
  ********************************************************/
 sim.scenario.initialState.objects = {
-  "1": {typeName: "Jumper", name:"James", shortLabel:"james"},
-  "2": {typeName: "Speaker", name:"Pedro", shortLabel:"pedro", barrier:"3"},
+  "1": {typeName: "Jumper", name:"jumper", shortLabel:"jumper"},
+  "2": {typeName: "Speaker", name:"speaker", barrier:"3"},
   "3": {typeName: "Barrier", name:"barrier", shortLabel:"bar", length:2},
   "4" : {typeName: "Matrix3_4", name:"matrix3_4", shortLabel:"mtrx", A1:0.25, A2:0.25, A3:0.25, A4:0.25,
   B1:0.25, B2:0.25, B3:0.25, B4:0.25, C1:0.25, C2:0.25, C3:0.25, C4:0.25}
@@ -50,29 +51,72 @@ sim.model.statistics = {
  ********************************************************/
 sim.config.imgFolder = "media/img/";
 sim.config.observationUI.type = "SVG";
-sim.config.observationUI.canvas.width = 600;
+sim.config.observationUI.canvas.width = 700;
 sim.config.observationUI.canvas.height = 300;
+//sim.config.observationUI.canvas.style = "background-color:yellow";
+
 sim.config.observationUI.fixedElements = {
-  "speaker": {
-    shapeName: "image",
-    shapeAttributes: { x: 350, y: 200, width: 50, height: 30},
-    style: "fill:brown; stroke-width:0"
-  }
+  "ground": { 
+      shapeName: "line",  
+      shapeAttributes: {  
+        x1: 10, y1: 260, 
+        x2: 610, y2: 260
+      },
+      style:"stroke:grey; stroke-width:10"
+  }    
 };
 sim.config.observationUI.objectViews = {
-  "serviceDesk1": [  // a view of the queue
-    { shapeName: "rect",  // a rectangle defined by
-      shapeAttributes: {  // left-upper corner (x,y) as well as width and height
-        x: function (sd) {return Math.max( 0, 330 - sd.queueLength * 20);},
-        width: function (sd) {return Math.min( 300, sd.queueLength * 20);},
-        y: 150, height: 80
+  "jumper": { 
+      shapeName: "image",  
+      shapeAttributes: {
+        id: "jumperView",
+        file: sim.config.imgFolder + "blind-man.png",
+        x: 10, y: 112,   // left-upper corner (x,y)
+        width: 154, height: 138
+      }
+  },
+  "barrier": [  
+    { shapeName: "rect",  
+      shapeAttributes: {  
+        x: 200, y: 200,  // left-upper corner (x,y) 
+        width: function (b) {return b.length * 50;},
+        height: 50
       },
-      style:"fill:yellow; stroke-width:0"
+      style:"fill:brown; stroke-width:0"
     },
     { shapeName: "text",
-      shapeAttributes: {x: 325, y: 250,
-          textContent: function (sd) {return sd.queueLength;}},
+      shapeAttributes: {x: 250, y: 190,
+          textContent: function (b) {return b.length;}},
       style:"font-size:14px; text-anchor:middle"
     }
-  ]
+  ],
+  "speaker": {
+      shapeName: "image",
+      shapeAttributes: { 
+        file: sim.config.imgFolder + "cartoon-man-wearing-suit.png", 
+        x: 500, y:119, width: 107, height: 131
+      }
+  }
+};
+sim.config.observationUI.eventAppearances = {
+  /* TODO: support temporary visibility of DOM elements
+  "Tell": {
+    //sound: {duration: 1000, source:"12/300/80 14/200/90"},
+    view: {  // an event view is a web animation of a DOM element
+      shapeName: "text",
+      shapeAttributes: {x: 500, y: 100,
+          textContent: function (e) {return e.lengthSymbol;}},
+      style:"font-size:14px; text-anchor:middle"
+      keyframes: [{color:'black'}, {color:'white'}],
+      duration: 1000,  // ms
+    }
+  },
+  */
+  "Jump": {
+    view: {  // an event view is a web animation of a DOM element
+      domElem: function () {return document.getElementById("jumperView");},  // the visualization container element
+      keyframes: [{x:100}, {x: function (e) {return e.jumpLength;}}],
+      duration: 1000  // ms
+    }
+  }
 };
