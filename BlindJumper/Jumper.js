2b2c969287d173135e42a7b32b66a40a05f98526
Yassir10 <sellal.yassir@gmail.com>
2019-09-10 13:08:51 +0200

    LearningMatrix ES6

--- BlindJumper/Jumper.js
+++ BlindJumper/Jumper.js
@@ -3,7 +3,6 @@ var Jumper = new cLASS({
   supertypeName: "oBJECT",
   properties: {
     "lengthSymbol": {range: "String", initialValue:"A"},
-<<<<<<< HEAD
     //"jumpLength": {range: "PositiveInteger"},
     /*"jumpSuccessProbMat" : {range: Array, initialValue:
           new LearningMatrix(new Array(new Array(0.25,0.25,0.25,0.25),
@@ -11,12 +10,7 @@ var Jumper = new cLASS({
     "tellSuccessProbMat" : {range: "Array", initialValue: [[0.25, 0.25, 0.25, 0.25], [0.25, 0.25, 0.25, 0.25],
         [0.25, 0.25, 0.25, 0.25]]},
     "position": {range:"PositiveInteger", minValue:0, maxValue:5},
-=======
-    "jumpLength": {range: "PositiveInteger", initialValue:1},
-    "jumpSuccessProbMat" : {range: "Array", initialValue:[[0.25, 0.25, 0.25, 0.25],
-        [0.25, 0.25, 0.25, 0.25], [0.25, 0.25, 0.25, 0.25]]},
-    "position": {range:"PositiveInteger", minValue:1, maxValue:5, shortLabel: "pos"},
->>>>>>> 1bfdb08bfcf7cba120989c2389d846b65b0c8210
+
   },
   methods: {
     "jump": function ( jumpLength) {
